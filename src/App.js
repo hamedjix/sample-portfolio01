43ae132ebdc0c6ff5d6d85b1470de07272c38b95
Hamed Jafari <hamedjix@gmail.com>
2021-04-14 16:23:04 +0430

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import AboutUs from './pages/AboutUs';
+//Import Components
 import Nav from './components/Nav';
+//import Pages
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUS';
+import OurWork from './pages/OurWork';
+import MovieDetails from './pages/MovieDetail';
 
 //import Style
 import GlobalStyle from './components/GlobalStyle';
+import { Route, Switch } from 'react-router';
+//Movie State
+import MovieState from './movieState';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path='/'>
+          <AboutUs />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+        <Route exact path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/work/:id'>
+          <MovieDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import { MovieState } from '../movieState';
+
+const MovieDetail = () => {
+  return (
+    <div>
+      <h1>Movie Details</h1>
+    </div>
+  );
+};
+
+export default MovieDetail;
