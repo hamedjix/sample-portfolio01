5f252f2f0ea1424d174dd00435dcd93913af5ea6
Hamed Jafari <hamedjix@gmail.com>
2021-04-15 12:05:03 +0430

    movie details page update

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,13 +1,34 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { MovieState } from '../movieState';
 
 const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  //useEffect
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovie(currentMovie[0]);
+    console.log(movie);
+  }, [movies, url]);
   return (
-    <div>
-      <h1>Movie Details</h1>
-    </div>
+    <>
+      {movie && (
+        <Details>
+          <Headline>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt='The Athlete' />
+          </Headline>
+        </Details>
+      )}
+    </>
   );
 };
+const Details = styled.div``;
+const Headline = styled.div``;
 
 export default MovieDetail;
